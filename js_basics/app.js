8932db887e2012b03b8b05465d05fe37aac1209f
곽진성[ 학부재학 / 통계학과 ] <jskwak98@korea.ac.kr>
2023-06-05 23:16:00 +0900

    Basic Conditionals Lessons

--- js_basics/app.js
+++ js_basics/app.js
@@ -16,4 +16,19 @@ const calculator = {
     },
 }
 
-console.log(calculator.power(2, 3));
+const age = parseInt(prompt("How old are you?")); //input + parsing
+// you can know the type of variable by, typeof age -> typeof blank var
+
+if (isNaN(age)) {
+    console.log("write number");
+} else if (age < 18) {
+    console.log("You too young for this");
+} else if (age >= 18 && age <= 50) {
+    console.log("You can drink this");
+} else if (age > 50 && age <= 90) {
+    console.log("Be careful about your health");
+} else if (age === 105){ //they use === as the equal, !== as inequal
+    console.log("Who YOu?")
+} else {
+    console.log("whatever")
+}
