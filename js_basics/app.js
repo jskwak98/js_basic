9fc460af89d21fc94808382405914c1b973e0ddc
Jinseong <jskwak98@naver.com>
2023-06-07 00:00:06 +0900

    Learned about document

--- js_basics/app.js
+++ js_basics/app.js
@@ -1,34 +1,9 @@
-const calculator = {
-    plus: function (a, b){
-        return a + b;
-    },
-    minus: function (a, b){
-        return a + b;
-    },
-    multiply: function (a, b){
-        return a * b;
-    },
-    divide: function (a, b){
-        return a / b;
-    },
-    power: function (a, b){
-        return a ** b;
-    },
-}
+//grab by id
+const title = document.getElementById("title");
+console.log(title);
+// <h1 id="title">Grab me!</h1>
+console.dir(title);
+//all attributes in the h1 object
+console.log(title.className);
 
-const age = parseInt(prompt("How old are you?")); //input + parsing
-// you can know the type of variable by, typeof age -> typeof blank var
-
-if (isNaN(age)) {
-    console.log("write number");
-} else if (age < 18) {
-    console.log("You too young for this");
-} else if (age >= 18 && age <= 50) {
-    console.log("You can drink this");
-} else if (age > 50 && age <= 90) {
-    console.log("Be careful about your health");
-} else if (age === 105){ //they use === as the equal, !== as inequal
-    console.log("Who YOu?")
-} else {
-    console.log("whatever")
-}
+title.innerText = "Change!" // change attribute by JS
