7b4cbcf10d10c7f567ae9d93548a0d2c92cd9699
Jinseong <jskwak98@naver.com>
2023-06-07 23:53:46 +0900

    Query Selector Learned

--- js_basics/app.js
+++ js_basics/app.js
@@ -1,9 +1,19 @@
-//grab by id
-const title = document.getElementById("title");
-console.log(title);
-// <h1 id="title">Grab me!</h1>
-console.dir(title);
-//all attributes in the h1 object
-console.log(title.className);
+const hellos = document.getElementsByClassName("hello");
+// get list by class name
+console.log(hellos);
+// HTMLCollection(5) [h1.hello, h1.hello, h1.hello, h1.hello, h1.hello]
 
-title.innerText = "Change!" // change attribute by JS
+const title = document.querySelector(".hellodiv h1");//cs selector
+console.log(title);
+// <h1>grab me by query selector 1</h1>
+// if multiple needed
+
+const titles = document.querySelectorAll(".hellodiv h1");//cs selector
+console.log(titles);
+//NodeList(3) [h1, h1, h1]0: h11: h12: h1length: 3[[Prototype]]: NodeList
+
+// you can use query selector for id
+
+const idSel = document.querySelector("#helloid");
+console.log(idSel);
+// <h3 id="helloid">grab me by id</h3>
