ad1c650564cb8f530c3c153dd395c5bfecf34022
jinseong <jskwak98@naver.com>
2023-06-09 23:33:13 +0900

    More Event used to handle changes

--- js_basics/event_handling.js
+++ js_basics/event_handling.js
@@ -11,5 +11,21 @@ function handleTitleClick(){
     }
 }
 
+function handleMouseEnter(){
+    title.style.color = "black";
+}
+
+function handleMouseLeave(){
+    title.style.color = "white";
+}
+
+
 title.addEventListener("click", handleTitleClick);
 //listens for event, need to tell js which event it listens to
+
+console.dir(title);
+//by looking at properties, you can see which event this object has
+// starts with 'on'
+
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
