99ebbecc6a16b7e2fa9ffa5a815157e5331282b4
jinseong <jskwak98@naver.com>
2023-06-10 20:59:28 +0900

    Learned classList, toggle

--- js_basics/css_in_javascript.js
+++ js_basics/css_in_javascript.js
@@ -1,14 +1,30 @@
 const title = document.querySelector(".eventHandlePractice");
 
 function handleTitleClick(){
-    //active string is called raw value and it can be the cause of error
-    const clickedClass = "clicked"
+    //"clicked" string is called raw value and it can be the cause of error
+    const clickedClass = "clicked";
     if (title.className === clickedClass){
         title.className = "eventHandlePractice";
     } else {
         title.className = clickedClass;
     }
-    
 }
 
-title.addEventListener("click", handleTitleClick);
+function handleTitleClickList(){
+    //Use class list for multiclass management
+    const clickedClass = "clicked";
+    //it checks clickedClass contained
+    if (title.classList.contains(clickedClass)){
+        title.classList.remove(clickedClass);
+    } else {
+        title.classList.add(clickedClass);
+    }
+}
+
+function handleTitleClickListToggle(){
+    //Use class list for multiclass management
+    title.classList.toggle("clicked");
+}
+
+console.dir(title);
+title.addEventListener("click", handleTitleClickListToggle);
