60b966b336e4ad68960198f8292f9e2ff0d5c45d
Jinseong <jskwak98@naver.com>
2023-06-12 16:23:14 +0900

    Added event argument on the listener and logged it

--- make/app.js
+++ make/app.js
@@ -12,9 +12,20 @@ function onLoginBtnClick(){
     } else if (val.length > 15){
         alert("Username is too long");
     } else {
-        console.log("hello", loginInput.value);
+        console.log("hello", val);
     }
 }
 
 //loginButton.addEventListener("click", onLoginBtnClick);
 
+function onLoginSubmit(event){
+    event.preventDefault();
+    // this stops the default behaviour of any event
+    // thus submission doesn't make the browser to refresh
+    console.log(event);
+    // you get information from browser when Submit event happens
+    const val = loginInput.value;
+    console.log("hello", val);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
