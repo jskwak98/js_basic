478d2f74460246ca2a1648fed949573756be36d7
Jinseong <jskwak98@naver.com>
2023-06-18 23:54:36 +0900

    Reorganize the code

---
+++ chrome_app/js/greetings.js
@@ -0,0 +1,36 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+const logoutBtn = document.querySelector("#logout");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLogoutClicked(event){
+    localStorage.removeItem("username");
+    location.reload();
+}
+
+logoutBtn.addEventListener("click", onLogoutClicked);
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+}
+
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}!`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
