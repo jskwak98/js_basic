ce172dedc18e23db302770eaa65a4d31027c8f21
Jinseong <jskwak98@naver.com>
2023-06-21 23:56:29 +0900

    Adding element to TODO list

--- chrome_app/js/todo.js
+++ chrome_app/js/todo.js
@@ -2,11 +2,20 @@ const todoForm = document.getElementById("todo-form");
 const todoList = document.getElementById("todo-list");
 const todoInput = todoForm.querySelector("input")
 
+function paintTodo(newTodo) {
+    //when you add todo this will paint new todo on page
+    const listElementWrapper = document.createElement("li");
+    const listElement = document.createElement("span");
+    listElementWrapper.appendChild(listElement);
+    listElement.innerText = newTodo;
+    todoList.appendChild(listElementWrapper);
+}
+
 function onTodoSubmit(event) {
     event.preventDefault();
     const newTodo = todoInput.value;
     todoInput.value = "";
-    
+    paintTodo(newTodo);
 }
 
 todoForm.addEventListener("submit", onTodoSubmit);
