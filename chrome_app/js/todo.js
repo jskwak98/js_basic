9ac2c678d68bc9069a0ad0c8879e8dac3f55e22e
jskwak98 <jskwak98@naver.com>
2023-06-22 23:40:22 +0900

    add remove button

--- chrome_app/js/todo.js
+++ chrome_app/js/todo.js
@@ -2,12 +2,27 @@ const todoForm = document.getElementById("todo-form");
 const todoList = document.getElementById("todo-list");
 const todoInput = todoForm.querySelector("input")
 
+function deleteTodo(event){
+    // to recognize which button is clicked among buttons
+    // get info from the event
+    const toDel = event.target.parentElement;
+    toDel.remove();
+}
+
 function paintTodo(newTodo) {
     //when you add todo this will paint new todo on page
     const listElementWrapper = document.createElement("li");
     const listElement = document.createElement("span");
-    listElementWrapper.appendChild(listElement);
     listElement.innerText = newTodo;
+
+    //create event listener for the list button
+    const listButton = document.createElement("button");
+    listButton.innerText = "❌";
+    listButton.addEventListener("click", deleteTodo);
+
+    //create element
+    listElementWrapper.appendChild(listElement);
+    listElementWrapper.appendChild(listButton);
     todoList.appendChild(listElementWrapper);
 }
 
